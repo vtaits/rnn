75bd3610111fa1d5cce6d6427e5d724ba0e5bcf6
Vadim Taits <taizvadim@gmail.com>
2024-07-18 13:08:51 +0300

    Fixed direction of last neuron in second layer

--- rnn_core/src/spiral/mod.rs
+++ rnn_core/src/spiral/mod.rs
@@ -2,12 +2,18 @@ use crate::structures::NetworkParams;
 
 pub fn get_next_field(params: &NetworkParams, field_x: usize, field_y: usize) -> (usize, usize) {
     let layer_2_to_1_x = if field_y % 2 == 0 {
+        // last field in row
         if field_x == params.layer_width - 1 {
-            field_x
+            if field_y == params.layer_height - 1 {
+                0
+            } else {
+                field_x
+            }
         } else {
             field_x + 1
         }
     } else {
+        // first field in row
         if field_x == 0 {
             field_x
         } else {
