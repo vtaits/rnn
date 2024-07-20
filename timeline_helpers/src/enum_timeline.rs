98420af812bb10c2e012e05e8f0a14a60890adc5
Vadim Taits <taizvadim@gmail.com>
2024-07-20 16:36:36 +0300

    Added enum timeline

---
+++ timeline_helpers/src/enum_timeline.rs
@@ -0,0 +1,112 @@
+use crate::{bits_to_number, number_to_bits};
+
+pub struct EnumTimelineParams<T> {
+    pub capacity: u8,
+    pub to_number: Box<dyn Fn(T) -> usize>,
+    pub to_option: Box<dyn Fn(usize) -> T>,
+}
+
+pub struct EnumTimeline<T> {
+    max_normalize_value: usize,
+    params: EnumTimelineParams<T>,
+}
+
+impl<T> EnumTimeline<T> {
+    pub fn new(params: EnumTimelineParams<T>) -> Self {
+        let max_normalize_value = 2usize.pow(params.capacity as u32) - 1;
+
+        EnumTimeline {
+            max_normalize_value,
+            params,
+        }
+    }
+
+    pub fn get_bits(&self, value: T) -> Vec<bool> {
+        let number = (self.params.to_number)(value);
+
+        number_to_bits(number, self.params.capacity, self.max_normalize_value)
+    }
+
+    pub fn reverse(&self, bits: &[bool]) -> T {
+        let number = bits_to_number(bits);
+
+        let option = (self.params.to_option)(number);
+
+        option
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_value_bits() {
+        let timeline = EnumTimeline::new(EnumTimelineParams {
+            capacity: 3,
+            to_number: Box::new(|value: &str| match value {
+                "one" => 1,
+                "two" => 2,
+                "three" => 3,
+                "four" => 4,
+                "five" => 5,
+                _ => 0,
+            }),
+            to_option: Box::new(|value| match value {
+                1 => "one",
+                2 => "two",
+                3 => "three",
+                4 => "four",
+                5 => "five",
+                _ => "zero",
+            }),
+        });
+
+        assert_eq!(timeline.get_bits("one"), vec![false, false, true],);
+
+        assert_eq!(timeline.get_bits("two"), vec![false, true, false],);
+
+        assert_eq!(timeline.get_bits("three"), vec![false, true, true],);
+
+        assert_eq!(timeline.get_bits("four"), vec![true, false, false],);
+
+        assert_eq!(timeline.get_bits("five"), vec![true, false, true],);
+
+        assert_eq!(timeline.get_bits("zero"), vec![false, false, false],);
+    }
+
+    #[test]
+    fn reverse() {
+        let timeline = EnumTimeline::new(EnumTimelineParams {
+            capacity: 3,
+            to_number: Box::new(|value: &str| match value {
+                "one" => 1,
+                "two" => 2,
+                "three" => 3,
+                "four" => 4,
+                "five" => 5,
+                _ => 0,
+            }),
+            to_option: Box::new(|value| match value {
+                1 => "one",
+                2 => "two",
+                3 => "three",
+                4 => "four",
+                5 => "five",
+                _ => "zero",
+            }),
+        });
+
+        assert_eq!(timeline.reverse(&[false, false, true]), "one",);
+
+        assert_eq!(timeline.reverse(&[false, true, false]), "two",);
+
+        assert_eq!(timeline.reverse(&[false, true, true]), "three",);
+
+        assert_eq!(timeline.reverse(&[true, false, false]), "four",);
+
+        assert_eq!(timeline.reverse(&[true, false, true]), "five",);
+
+        assert_eq!(timeline.reverse(&[false, false, false]), "zero",);
+    }
+}
--- timeline_helpers/src/lib.rs
+++ timeline_helpers/src/lib.rs
@@ -1,9 +1,11 @@
 mod bits_to_number;
+mod enum_timeline;
 mod float_timeline;
 mod integer_timeline;
 mod number_to_bits;
 
 pub use bits_to_number::bits_to_number;
+pub use enum_timeline::{EnumTimeline, EnumTimelineParams};
 pub use float_timeline::{FloatTimeline, FloatTimelineParams};
 pub use integer_timeline::{IntegerTimeline, IntegerTimelineParams};
 pub use number_to_bits::number_to_bits;
