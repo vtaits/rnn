3c0645f7afdb57b627dd066261a82f9516489caa
Vadim Taits <taizvadim@gmail.com>
2024-11-05 21:38:35 +0300

    Added tests for not restoring missed bits

--- rnn_core/tests/predictions.rs
+++ rnn_core/tests/predictions.rs
@@ -84,9 +84,50 @@ fn restore_missed_bits(#[case] full: Vec<bool>, #[case] cut: Vec<bool>, #[case] 
 
     network.push_data_binary(&full);
 
-    println!("PREDICT");
-
     let result = network.predict(&cut);
 
     assert_eq!(result, full);
+}
+
+#[rstest]
+#[case(
+    vec![true, true, true, true, true, false],
+    vec![true, false, true, false, true, false],
+    3.0,
+)]
+#[case(
+    vec![true, true, true, true, true, false],
+    vec![false, false, false, false, true, true],
+    1.0,
+)]
+fn not_restore_missed_bits(#[case] full: Vec<bool>, #[case] cut: Vec<bool>, #[case] alpha: f32) {
+    let mut network = Network::new(
+        LayerParams {
+            field_width: 3,
+            field_height: 2,
+            layer_width: 2,
+            layer_height: 2,
+        },
+        SynapseParams {
+            alpha,
+            gamma: 0.5,
+            g_dec: 0.0,
+            g_inc: 10.0,
+            g_0: 1.0,
+            max_g: 10.0,
+            initial_strong_g: 7.0,
+            h: 3,
+            refract_interval: 3,
+            threshold: 0.9,
+            signal_shift_interval: 2,
+            signal_rest_shift_limit: Some(0),
+        },
+        None,
+    );
+
+    network.push_data_binary(&full);
+
+    let result = network.predict(&cut);
+
+    assert_eq!(result, cut);
 }
