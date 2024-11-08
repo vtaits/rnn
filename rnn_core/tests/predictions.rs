8381fb843216fdddb66504ccfe27df53fc6a6e61
Vadim Taits <taizvadim@gmail.com>
2024-11-08 21:14:59 +0300

    Added test for separated sequences

--- rnn_core/tests/predictions.rs
+++ rnn_core/tests/predictions.rs
@@ -136,32 +136,76 @@ fn not_restore_missed_bits(#[case] full: Vec<bool>, #[case] cut: Vec<bool>, #[ca
 #[case(
     vec![
         (
-            vec![true, true, true, true, true, true, false, false, false],
-            vec![true, false, true, false, true, true, false, false, false],
+            vec![
+                true, true, true, true,
+                false, false, false, false,
+                false, false, false, false,
+                false, false, false, false,
+            ],
+            vec![
+                true, false, true, true,
+                false, false, false, false,
+                false, false, false, false,
+                false, false, false, false,
+            ],
         ),
         (
-            vec![true, true, true, false, false, false, true, true, true],
-            vec![true, false, true, false, false, false, false, true, true],
+            vec![
+                false, false, false, false,
+                true, true, true, true,
+                false, false, false, false,
+                false, false, false, false,
+            ],
+            vec![
+                false, false, false, false,
+                true, false, true, true,
+                false, false, false, false,
+                false, false, false, false,
+            ],
         ),
         (
-            vec![false, false, false, true, true, true, true, true, true],
-            vec![false, false, false, true, false, true, false, true, true],
+            vec![
+                false, false, false, false,
+                false, false, false, false,
+                true, true, true, true,
+                false, false, false, false,
+            ],
+            vec![
+                false, false, false, false,
+                false, false, false, false,
+                true, false, true, true,
+                false, false, false, false,
+            ],
+        ),
+        (
+            vec![
+                false, false, false, false,
+                false, false, false, false,
+                false, false, false, false,
+                true, true, true, true,
+            ],
+            vec![
+                false, false, false, false,
+                false, false, false, false,
+                false, false, false, false,
+                true, false, true, true,
+            ],
         ),
     ],
 )]
-fn restore_multiple_sequences(#[case] sequences: Vec<(Vec<bool>, Vec<bool>)>) {
+fn restore_multiple_separated_sequences(#[case] sequences: Vec<(Vec<bool>, Vec<bool>)>) {
     let mut network = Network::new(
         LayerParams {
-            field_width: 3,
-            field_height: 3,
+            field_width: 4,
+            field_height: 4,
             layer_width: 3,
             layer_height: 3,
         },
         SynapseParams {
-            alpha: 3.0,
+            alpha: 2.0,
             gamma: 0.5,
             g_dec: 0.0,
-            g_inc: 10.0,
+            g_inc: 5.0,
             g_0: 1.0,
             max_g: 10.0,
             initial_strong_g: 7.0,
@@ -173,6 +217,98 @@ fn restore_multiple_sequences(#[case] sequences: Vec<(Vec<bool>, Vec<bool>)>) {
         },
         None,
     );
+
+    for sequence in sequences.iter() {
+        network.push_data_binary(&sequence.0);
+    }
+
+    for sequence in sequences.iter() {
+        network.push_data_binary(&sequence.0);
+    }
+
+    for sequence in sequences.iter() {
+        println!("PREDICT");
+        let result = network.predict(&sequence.1);
+
+        assert_eq!(result, sequence.0);
+    }
+}
+
+#[rstest]
+#[case(
+    vec![
+        (
+            vec![
+                true, true, true, true,
+                true, true, true, true,
+                false, false, false, false,
+                false, false, false, false,
+            ],
+            vec![
+                true, false, true, true,
+                false, true, true, true,
+                false, false, false, false,
+                false, false, false, false,
+            ],
+        ),
+        (
+            vec![
+                false, false, false, false,
+                true, true, true, true,
+                true, true, true, true,
+                false, false, false, false,
+            ],
+            vec![
+                false, false, false, false,
+                true, false, true, true,
+                false, true, true, true,
+                false, false, false, false,
+            ],
+        ),
+        (
+            vec![
+                false, false, false, false,
+                false, false, false, false,
+                true, true, true, true,
+                true, true, true, true,
+            ],
+            vec![
+                false, false, false, false,
+                false, false, false, false,
+                true, false, true, true,
+                false, true, true, true,
+            ],
+        ),
+    ],
+)]
+fn restore_multiple_overlapping_sequences(#[case] sequences: Vec<(Vec<bool>, Vec<bool>)>) {
+    let mut network = Network::new(
+        LayerParams {
+            field_width: 4,
+            field_height: 4,
+            layer_width: 3,
+            layer_height: 3,
+        },
+        SynapseParams {
+            alpha: 2.0,
+            gamma: 0.5,
+            g_dec: 0.0,
+            g_inc: 5.0,
+            g_0: 1.0,
+            max_g: 10.0,
+            initial_strong_g: 7.0,
+            h: 3,
+            refract_interval: 3,
+            threshold: 0.8,
+            signal_shift_interval: 2,
+            signal_rest_shift_limit: Some(0),
+        },
+        None,
+    );
+
+    for sequence in sequences.iter() {
+        network.push_data_binary(&sequence.0);
+    }
 
     for sequence in sequences.iter() {
         network.push_data_binary(&sequence.0);
