99107638ef5dad3aa7de0fa3503bdde695be6e54
Vadim Taits <taizvadim@gmail.com>
2025-06-17 17:51:18 +0300

    cross-validation

--- count_accuracy/README.md
+++ count_accuracy/README.md
@@ -1,4 +1,4 @@
 * `CONFIG_DIR` - not required, path to directory with network config
 * `CONFIG_PATH` - path to network config
-* `START_MEASURE_INDEX` - start index for measurement in datasets
-* `END_MEASURE_INDEX` - end index for measurement in datasets
+* `MEASURE_DATA_LENGTH` - size of data or measurement
+* `MEASURES_COUNT` - number of measurements with differnt offsets
--- count_accuracy/src/main.rs
+++ count_accuracy/src/main.rs
@@ -6,30 +6,52 @@ use rnn_instance::{init_data_layer_by_env, InitDataLayerParams};
 
 #[tokio::main]
 async fn main() -> Result<(), ()> {
-    let start_index = env::var("START_MEASURE_INDEX")
-        .expect("START_MEASURE_INDEX should be setted")
+    let measure_data_length = env::var("MEASURE_DATA_LENGTH")
+        .expect("MEASURE_DATA_LENGTH should be setted")
         .parse::<usize>()
-        .expect("START_MEASURE_INDEX should be a number");
-    let end_index = env::var("END_MEASURE_INDEX")
-        .expect("END_MEASURE_INDEX should be setted")
+        .expect("MEASURE_DATA_LENGTH should be a number");
+    let measures_count = env::var("MEASURES_COUNT")
+        .expect("MEASURES_COUNT should be setted")
         .parse::<usize>()
-        .expect("END_MEASURE_INDEX should be a number");
+        .expect("MEASURES_COUNT should be a number");
 
-    let (mut data_layer, measurement_data) = init_data_layer_by_env(&InitDataLayerParams {
-        train: true,
-        start_index: None,
-        end_index: None,
-        end_measurement_index: Some(end_index),
-        start_measurement_index: Some(start_index),
-    });
+    let mut total_positive = 0;
+    let mut total_negative = 0;
 
-    let (positive, negative) = data_layer.count_accuracy(measurement_data);
+    for page in 0..measures_count {
+        println!("Try #{}", page + 1);
+
+        let start_index = page * measure_data_length;
+        let end_index = start_index + measure_data_length;
+
+        let (mut data_layer, measurement_data) = init_data_layer_by_env(&InitDataLayerParams {
+            train: true,
+            start_index: None,
+            end_index: None,
+            end_measurement_index: Some(end_index),
+            start_measurement_index: Some(start_index),
+        });
+
+        let (positive, negative) = data_layer.count_accuracy(measurement_data);
+
+        total_positive += positive;
+        total_negative += negative;
+
+        println!(
+            "positive: {}, negative: {}, total: {}",
+            positive,
+            negative,
+            positive + negative
+        );
+    }
+
+    println!("Total");
 
     println!(
         "positive: {}, negative: {}, total: {}",
-        positive,
-        negative,
-        positive + negative
+        total_positive,
+        total_negative,
+        total_positive + total_negative
     );
 
     Ok(())
